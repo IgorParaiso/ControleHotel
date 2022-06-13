39456322b47cf3c52acad143ac649d16c600fa82
IgorParaiso <igorparaisod@gmail.com>
2022-06-12 23:02:11 -0300

    bug fix

--- HotelLpoo/src/gravacao/LerArquivo.java
+++ HotelLpoo/src/gravacao/LerArquivo.java
@@ -7,6 +7,7 @@ import usuario.Usuario;
 
 public class LerArquivo {
     private Scanner entrada;
+    private int num;
     
     public LerArquivo (String nome) throws FileNotFoundException{
         try {
